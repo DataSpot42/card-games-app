61c73e6427161783cb9be6efa4ef655cde9667c7
DataSpot42 <stevelookingforward@gmail.com>
2023-10-15 20:14:25 +0100

    tweaked homepage
    Signed-off-by: DataSpot42 <stevelookingforward@gmail.com>

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,9 +12,8 @@ const Home=() => {
             <p>New games will be added over time, for now try 21!</p>
             <button className="dealMe" onClick={()=> navigate('/TwentyOne')}>21 Game</button>
             
-            <li>Click Deal to take 2 cards, take more if you wish</li>
-            <li>Near as you dare to 21? Press STICK to save score</li>
-            <li>Keep dealing until you run out of cards to get high score</li>
+            <li>Play with all 52 cards to get a high score</li>
+            
         </div>
     )
 }
